a27e23019e1614efd13a8efa79d50ef30f304643
Feruza <ergashevaferuzabonuu@gmail.com>
2024-07-30 12:20:39 +0500

    qwe

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Some"),
+        title: const Text("qqw"),
       ),
       body: const Center(
         child: Text("Something"),
